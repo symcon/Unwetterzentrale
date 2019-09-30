6b7dea76f8b2b2c0f795815a7d1c9bfba8136185
Till Brede <tb@symcon.de>
2019-09-30 15:45:37 +0200

    added validation test (PHP-66)

---
+++ tests/UnwetterzentraleValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class UnwetterValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateUnwetterzentrale(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateUnwetterzentraleModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Unwetterzentrale');
+    }
+}
